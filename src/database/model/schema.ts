3207b8f36eedd88394230a45f91bb7c4043d03c4
Clinton Lee <clintonleex@gmail.com>
2024-12-19 00:30:08 +0100

    Started setting up schema; thinking about image sync

--- src/database/model/schema.ts
+++ src/database/model/schema.ts
@@ -4,13 +4,54 @@ export default appSchema({
     version: 1,
     tables: [
         tableSchema({
-            name: "items",
+            name: "training_event",
             columns: [
-                { name: "content", type: "string" },
                 { name: "created_by", type: "string" },
                 { name: "created_at", type: "number" },
                 { name: "updated_at", type: "number" },
+                { name: "scheduled_for", type: "number" },
+                { name: "completed_at", type: "number" },
+                { name: "location", type: "string" },
             ],
         }),
+        tableSchema({
+            name: "participation",
+            columns: [
+                { name: "created_by", type: "string" },
+                { name: "created_at", type: "number" },
+                { name: "updated_at", type: "number" },
+                { name: "training_event_id", type: "string" },
+                { name: "client_id", type: "string" },
+                { name: "participant_id", type: "string" },
+            ],
+        }),
+        tableSchema({
+            name: "training_event_image",
+            columns: [
+                { name: "created_by", type: "string" },
+                { name: "created_at", type: "number" },
+                { name: "updated_at", type: "number" },
+                { name: "synced_at", type: "number" },
+                { name: "training_event_id", type: "string" },
+            ],
+        }),
+        tableSchema({
+            name: "participant_list_image",
+            columns: [
+                { name: "created_by", type: "string" },
+                { name: "created_at", type: "number" },
+                { name: "updated_at", type: "number" },
+                { name: "synced_at", type: "number" },
+                { name: "training_event_id", type: "string" },
+            ],
+        }),
+        // Split these up into separate schema files and merge in this one?
+        // Participants that aren't clients
+        // Clients
+        // Villages
+        // Zones
+        // Districts
+        // Countries
+        // Staff
     ],
 });
