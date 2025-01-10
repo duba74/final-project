6a74e534430ceb9ebbce0c6282c5534e7da97bbe
Clinton Lee <clintonleex@gmail.com>
2025-01-10 01:14:11 +0100

    new rn app with yarn for pm, resolved expo-router import problem

---
+++ training/src/database/database.ts
@@ -0,0 +1,39 @@
+import { Database } from "@nozbe/watermelondb";
+import { randomUUID } from "expo-crypto";
+
+import schema from "./data-model/schema";
+import migrations from "./data-model/migrations";
+import { createAdapter } from "./adapter";
+import { setGenerator } from "@nozbe/watermelondb/utils/common/randomId";
+
+import TrainingModule from "./data-model/models/TrainingModule";
+import TrainingEvent from "./data-model/models/TrainingEvent";
+import Village from "./data-model/models/Village";
+import Zone from "./data-model/models/Zone";
+import District from "./data-model/models/District";
+import Country from "./data-model/models/Country";
+
+setGenerator(() => randomUUID());
+
+const database = new Database({
+    adapter: createAdapter({ schema /*, migrations*/ }),
+    modelClasses: [
+        TrainingModule,
+        TrainingEvent,
+        Village,
+        // Zone,
+        // District,
+        // Country,
+    ],
+});
+
+export default database;
+
+export const trainingModuleCollection =
+    database.get<TrainingModule>("training_module");
+export const trainingEventCollection =
+    database.get<TrainingEvent>("training_event");
+export const villageCollection = database.get<Village>("village");
+// export const zoneCollection = database.get<Zone>("zone");
+// export const districtCollection = database.get<District>("district");
+// export const countryCollection = database.get<Country>("country");
---
+++ training/src/database/secondary-sync.ts
@@ -0,0 +1,43 @@
+import { synchronize } from "@nozbe/watermelondb/sync";
+import database from "./database";
+import { Platform } from "react-native";
+
+const secondarySync = async () => {
+    const host =
+        Platform.OS === "web"
+            ? "http://127.0.0.1:8000"
+            : "https://d2e1-197-234-221-131.ngrok-free.app";
+
+    await synchronize({
+        database,
+        pullChanges: async ({
+            lastPulledAt /*, schemaVersion, migration*/,
+        }) => {
+            console.log(
+                `🍉 Secondary sync - Attempting pull with lastPulledAt = ${lastPulledAt}`
+            );
+
+            const urlParams = `lastPulledAt=${lastPulledAt}`;
+            // const urlParams = `lastPulledAt=${lastPulledAt}&schemaVersion=${schemaVersion}&migration=${migration}`;
+            const url = `${host}/api/secondary-sync/?${urlParams}`;
+            console.log(url);
+
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(await response.text());
+            }
+
+            // console.log(await response.text());
+            const { changes, timestamp } = await response.json();
+
+            console.log(`🍉 Pull succeeded at timestamp = ${timestamp}`);
+            console.log(`🍉 Pull succeeded with changes:`);
+            // console.log(changes);
+
+            return { changes, timestamp };
+        },
+        sendCreatedAsUpdated: true,
+    });
+};
+
+export default secondarySync;
