38091b405f2b3bbfa667de6fd14aa6fbb0458336
clinton <clintonleex@gmail.com>
2025-02-04 10:10:25 +0100

    accordion of training events under village

--- training/src/components/village-list/VillageList.tsx
+++ training/src/components/village-list/VillageList.tsx
@@ -4,6 +4,12 @@ import { withObservables } from "@nozbe/watermelondb/react";
 import { FlatList, StyleSheet } from "react-native";
 import VillageListItem from "./VillageListItem";
 
+const trainingEvents = [
+    { id: "1", date: "121", time: "323", status: "not done" },
+    { id: "2", date: "881", time: "029", status: "done" },
+    { id: "3", date: "992", time: "838", status: "canceled" },
+];
+
 type VillageListProps = { villages: Village[] };
 
 const VillageList = ({ villages }: VillageListProps) => {
@@ -11,7 +17,12 @@ const VillageList = ({ villages }: VillageListProps) => {
         <FlatList
             data={villages}
             keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <VillageListItem village={item} />}
+            renderItem={({ item }) => (
+                <VillageListItem
+                    village={item}
+                    trainingEvents={trainingEvents}
+                />
+            )}
         />
     );
 };
--- training/src/components/village-list/VillageListItem.tsx
+++ training/src/components/village-list/VillageListItem.tsx
@@ -1,13 +1,48 @@
 import Village from "@/database/data-model/models/Village";
 import { withObservables } from "@nozbe/watermelondb/react";
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-type VillageListItemProps = { village: Village };
+type trainingEvent = {
+    id: string;
+    date: string;
+    time: string;
+    status: string;
+};
+type VillageListItemProps = {
+    village: Village;
+    trainingEvents: trainingEvent[];
+};
 
-const VillageListItem = ({ village }: VillageListItemProps) => {
+const VillageListItem = ({ village, trainingEvents }: VillageListItemProps) => {
+    const [isExpanded, setExpanded] = useState<boolean>(false);
+
+    const toggleExpanded = () => {
+        setExpanded(!isExpanded);
+    };
+
     return (
         <View>
             <Text>{village.name}</Text>
+            <Pressable onPress={toggleExpanded}>
+                <Text>{isExpanded ? "▲" : "▼"}</Text>
+            </Pressable>
+            {isExpanded && (
+                <View>
+                    <Text>Training Events show here...</Text>
+                    {/* {trainingEvents.length > 0 ? (
+                        trainingEvents.map((event) => (
+                            <View key={event.id} >
+                                <Text>Date: {event.date}</Text>
+                                <Text>Time: {event.time}</Text>
+                                <Text>Status: {event.status}</Text>
+                            </View>
+                        ))
+                    ) : (
+                        <Text>No training events available</Text>
+                    )} */}
+                </View>
+            )}
         </View>
     );
 };
