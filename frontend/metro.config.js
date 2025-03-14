9695fc222914b69cefa13f35000bd87527cb3870
duba74 <70065810+duba74@users.noreply.github.com>
2025-03-14 22:02:59 +0100

    Delete frontend directory

--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-    // Additional features...
-});
-
-module.exports = config;
--- frontend/src/app/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-import { useSession } from "../hooks/ctx";
-
-const SignIn = () => {
-    const { signIn } = useSession();
-};
-
-export default SignIn;
